c542a46b307e8c104581b462c30ae30d792bc3dd
sbcreel <creelins@gmail.com>
2021-06-08 18:02:00 -0400

    updating case_statement file with more exercises

--- case_statement.rb
+++ case_statement.rb
@@ -1,12 +1,65 @@
 # case_statement.rb
 
+=begin
 a = 5
 
-case a
+answer = case a
 when 5
-  puts "a is 5"
+  "a is 5"
 when 6
-  puts "a is 6"
+ "a is 6"
 else  
-  puts "a is neither 5, nor 6"
+ "a is neither 5, nor 6"
 end
+
+puts answer
+
+=end
+
+=begin
+a = "xxxxx"
+
+answer = case a
+when "hello"
+  "hello"
+when "goodbye"
+  "goodbye"
+when "be right back"
+  "be right back"
+else  
+  "see you later"
+end
+
+puts answer
+=end
+
+=begin  
+a = false
+if a
+  puts "how can this be true?"
+else  
+  puts "it is not true"
+end
+=end
+
+=begin
+puts "what is your string?"
+answer = gets.chomp
+
+if answer.length > 10
+  puts "#{answer.upcase}"
+end
+=end
+
+def capitalize(string)
+  if string.length > 10
+    string.upcase
+  else
+    string
+  end
+end
+
+
+  puts capitalize('scott creel')
+  puts capitalize('scott')
+  
