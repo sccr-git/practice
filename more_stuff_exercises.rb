01412f7e3283ede46978b465d0d14e75ed58b32e
sbcreel <creelins@gmail.com>
2021-06-28 08:13:17 -0400

    exercises form the 'more stuff' section

---
+++ more_stuff_exercises.rb
@@ -0,0 +1,15 @@
+=begin
+def check_lab?(word)
+ if word =~ /lab/
+  puts word
+ else
+  puts "not a match!"
+ end
+end
+
+ check_lab?("laboratory")
+ check_lab?("experiment")
+ check_lab?("Pans Labyrinth")
+ check_lab?("elaborate")
+ check_lab?("polar bear")
+ =end
