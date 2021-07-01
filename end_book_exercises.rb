300699796bee6fe231d6a432595c0c8b00e58430
sbcreel <creelins@gmail.com>
2021-07-01 07:28:31 -0400

    update end_book_exercises.rb

--- end_book_exercises.rb
+++ end_book_exercises.rb
@@ -60,10 +60,16 @@ p h
 
 =end
 
+=begin
 contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
             ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
 
 contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+
+contacts.map do |k,v|
+  k
+
+=end
 
 # Expected output:
 #  {
@@ -102,6 +108,7 @@ end
 p new_array.flatten
 =end
 
+=begin
 hash1 = {shoes: "nike", "hat" => "adidas", :hoodie => true}
 hash2 = {"hat" => "adidas", :shoes => "nike", hoodie: true}
 
@@ -110,4 +117,17 @@ if hash1 == hash2
 else
   puts "These hashes are not the same!"
 end
+=end
 
+
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+fields = [:email, :address, :phone]
+
+contacts.each_with_index do |(name,hash), idx|
+  fields.each do |field|
+    hash[field] = contact_data[idx].shift
+  end
+end
+
+p contacts
